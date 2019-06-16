e38a90f45a7e8acf7c9776ce229cc666a039ca16
Thi6 <thi.dnguyen96@gmail.com>
2019-06-16 09:02:35 +0100

    getAllRecipes for rest layer written and tested

--- src/main/java/com/bae/REST/RecipeController.java
+++ src/main/java/com/bae/REST/RecipeController.java
@@ -1,5 +1,22 @@
 package com.bae.REST;
 
-public class RecipeController {
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+import com.bae.business.RecipeService;
+
+@Path("/recipe")
+public class RecipeController {
+	@Inject
+	RecipeService recipeService;
+	
+	@Path("/getAllRecipes")
+	@GET
+	@Produces({"application/json"})
+	public String getAllRecipes() {
+		return recipeService.getAllRecipes();
+	}
+		
 }
