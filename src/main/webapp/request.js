e193ed5e12b08fc0a32dfef88215f83c1061da35
Thi6 <thi.dnguyen96@gmail.com>
2019-06-21 12:59:09 +0100

    changed ip address

--- src/main/webapp/request.js
+++ src/main/webapp/request.js
@@ -1,7 +1,7 @@
-// const ipAddress = "35.228.193.58:8888";
+const ipAddress = "34.65.43.71:8888";
 
 //ip address for local host
-const ipAddress = "localhost:8080";
+// const ipAddress = "localhost:8080";
 
 const makeRequest = (method, url, body) => {
 
